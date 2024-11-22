8308b5863d59dbba878c69185866723d0c927f20
Ash <154726037+aarij0119@users.noreply.github.com>
2024-11-22 12:45:58 +0530

    add admin panel

--- routes/ownerrouter.js
+++ routes/ownerrouter.js
@@ -2,6 +2,9 @@ const express = require('express');
 const app = express();
 const router = express.Router()
 const ownerModel = require('../models/owner');
+const bcrypt = require('bcrypt');
+const isloggedin = require('../middleware/isloggedin');
+
 
 // console.log(process.env.NODE_ENV)
 
@@ -15,14 +18,18 @@ if (process.env.NODE_ENV === "development") {
             if (owner.length > 0) {
                 return res.status(403).send("You don't have permission to access this route.");
             }
-            const createdOwner = await ownerModel.create({
-                fullname: "Riyaz",
-                email: "riyaz@gmail.com",
-                password: 123456789,
-                gstin: "12werevbnc"
-            });
+            bcrypt.genSalt(10, function (err, salt) {
+                bcrypt.hash("123456", salt, async function (err, hash) {
+                    const createdOwner = await ownerModel.create({
+                        fullname: "Riyaz",
+                        email: "riyaz@gmail.com",
+                        password: 123456789 + hash,
+                        gstin: "12werevbnc"
+                    });
+                    res.status(201).json({ message: "Owner created successfully", owner: createdOwner });
+                })
+            })
 
-            res.status(201).json({ message: "Owner created successfully", owner: createdOwner });
         } catch (err) {
             console.error(err.message);
             res.status(500).send("An error occurred.");
@@ -30,11 +37,11 @@ if (process.env.NODE_ENV === "development") {
     });
 }
 
-
-router.get('/', function (req, res) {
-    res.send("Owner Route")
+router.get('/admin', function (req, res) {
+    res.render('admin')
 
 });
+
 
 
 module.exports = router
